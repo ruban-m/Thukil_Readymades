0644069196c36d300cbf5af670de94069dde520f
ruban-m <rubantamilboy@gmail.com>
2022-03-09 15:43:44 +0530

    new changes

--- js/script.js
+++ js/script.js
@@ -2,18 +2,20 @@ function whatsapp() {
   window.location = 'https://wa.me/+919787434087'
 }
 // slideshow
-var i = 0
-var path = new Array()
-path[0] = './slide-show/1.jpg'
-path[1] = './slide-show/2.jpg'
-path[2] = './slide-show/3.jpg'
-path[3] = './slide-show/4.jpg'
-path[4] = './slide-show/5.jpg'
-path[5] = './slide-show/6.jpg'
-function swapimage() {
-  document.slider.src = path[i]
-  if (i < path.length - 1) i++
-  else i = 0
-  setTimeout('swapimage()', 4000)
+let shttp = new XMLHttpRequest()
+shttp.open('GET', './json/slide.json', false)
+shttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    let s = JSON.parse(this.responseText)
+    let i = 0
+    setInterval(() => {
+      if (i < s.length) {
+        document.slider.src = s[i]['path']
+        i++
+      } else {
+        i = 0
+      }
+    }, 4000)
+  }
 }
-window.onload = swapimage
+shttp.send()
